20373adb640140be15ea94a1c12a9aadfca7fd9e
NgoTrungHieu <ngotrunghieu2k@gmail.com>
2022-08-12 00:49:47 +0700

    create auth

---
+++ server/src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserSchema } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
---
+++ server/src/auth/user.entity.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+export type UserDocument = User & Document;
+
+@Schema({ timestamps: true })
+export class User extends Document {
+  @Prop({ required: true, unique: true, trim: true })
+  username: string;
+
+  @Prop({ required: true, unique: true, trim: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true, trim: true })
+  firstName: string;
+
+  @Prop({ required: true, trim: true })
+  lastName: string;
+
+  @Prop()
+  avatar: string;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Diary' }] })
+  diaries: mongoose.Schema.Types.ObjectId[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre<User>('save', function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = hash;
+    next();
+  });
+});
+
+UserSchema.methods.comparePassword = function (password: string) {
+  return bcrypt.compareSync(password, this.password);
+};
