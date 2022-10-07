17636bd2279637e710062098dbb6c4402fdc6858
Ngo Hieu <ngotrunghieu2k@gmail.com>
2022-10-07 20:41:29 +0700

    fix dispatch

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DiaryModule } from './diary/diary.module';
+
+const FRONTEND_PATH = join(__dirname, '..', '..', 'client', 'build');
 
 @Module({
   imports: [
@@ -16,6 +20,18 @@ import { DiaryModule } from './diary/diary.module';
       }),
       inject: [ConfigService],
     }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: FRONTEND_PATH,
+    //   exclude: ['nth/api/v1*'],
+    //   serveStaticOptions: {
+    //     setHeaders: function (res, path) {
+    //       // Prevent catching of the frontend files
+    //       if (path === join(FRONTEND_PATH, 'index.html')) {
+    //         res.setHeader('Cache-control', 'public, max-age=0');
+    //       }
+    //     }
+    //   }
+    // }),
     AuthModule,
     DiaryModule,
   ],
